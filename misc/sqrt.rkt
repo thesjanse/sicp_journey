5606f524cf63082ef380b6b46eb3363d60aa0a68
Alexander Mejinski <alexander.mejinski@gmail.com>
2020-04-19 13:18:54 +0300

    refactor: block structure instead of separate functions

--- misc/sqrt.rkt
+++ misc/sqrt.rkt
@@ -1,25 +1,26 @@
 #lang racket
 
 
-(define (sqrt-iter guess x)
+(define (sqrt x)
+    (define (average a b)
+        (/ (+ a b) 2))
+
+    (define (square a)
+        (* a a))
+
+    (define (improve guess x)
+        (average guess (/ x guess)))
+
+    (define (good-enough? guess x)
+        (= (improve guess x) guess))
+
+    (define (sqrt-iter guess x)
     (if (good-enough? guess x)
         guess
         (sqrt-iter (improve guess x)
                     x)))
 
-(define (improve guess x)
-    (average guess (/ x guess)))
-
-(define (average x y)
-    (/ (+ x y) 2))
-
-(define (good-enough? guess x)
-    (= (improve guess x) guess))
-
-(define (sqrt x)
     (sqrt-iter 1.0 x))
 
-(define (square x)
-    (* x x))
 
 (sqrt 9)
